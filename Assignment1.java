fd463140124ce2c844a99ce20b24299373e0bce4
sumit2k7 <sumit2k7@users.noreply.github.com>
2016-04-19 16:24:48 +0530

    Added files via upload

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args)
+	{
+		int iFirstNo=10;
+		float fSecondNo=40;
+		int sum;
+		sum=iFirstNo+(int)fSecondNo;
+		System.out.println("Sum of " + iFirstNo + " & " + fSecondNo + " is :->" + sum);
+		
+	}
+}
